0e5506b07c08ca0cc42773bb255ce247d5cfe8d4
Sad-Duck <sepens@yandex.ru>
2023-02-27 14:46:55 +0300

    style: переименовать SatatsClient

--- stats/stats-client/src/main/java/ru/practicum/StatsClientApp.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClientApp.java
@@ -1,44 +1,13 @@
 package ru.practicum;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-
+@SpringBootApplication
 public class StatsClientApp {
 
-    protected final RestTemplate rest;
-
-    final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-    public StatsClientApp(@Value("${stats-client-server.url}") String serverUrl, RestTemplateBuilder builder) {
-        this.rest = builder
-                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
-                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
-                .build();
+    public static void main(String[] args) {
+        SpringApplication.run(StatsClientApp.class, args);
     }
 
-    protected ResponseEntity<List<ViewStatsDto>> get(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
-        StringBuilder uriBuilder = new StringBuilder();
-        for (String uri : uris) {
-            uriBuilder.append("uris=").append(uri).append("&");
-        }
-        String query = String.format("/stats?start=%s&end=%s&%sunique=%s}", start.format(formatter), end.format(formatter), uriBuilder, unique);
-        return rest.exchange(query, HttpMethod.GET, null, new ParameterizedTypeReference<>() {
-        });
-    }
-
-    protected void post(EndPointHitDto endPointHit) {
-        HttpEntity<EndPointHitDto> requestEntity = new HttpEntity<>(endPointHit);
-        rest.exchange("/hit", HttpMethod.POST, requestEntity, EndPointHitDto.class);
-    }
 }
