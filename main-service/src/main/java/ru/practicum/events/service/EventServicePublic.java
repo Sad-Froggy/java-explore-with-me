397b5ee1dc03f814e187dc301853052a91ebf4b8
Alexey <sepens@yandex.ru>
2023-03-16 01:27:43 +0300

    fix: ненужные аннотации

--- main-service/src/main/java/ru/practicum/events/service/EventServicePublic.java
+++ main-service/src/main/java/ru/practicum/events/service/EventServicePublic.java
@@ -1,6 +1,5 @@
 package ru.practicum.events.service;
 
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.events.dto.EventFullDto;
 import ru.practicum.events.dto.EventPublicSearch;
 
@@ -8,9 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public interface EventServicePublic {
-    @Transactional
+
     List<EventFullDto> getEventsWithFilters(EventPublicSearch ev, String ip);
 
-    @Transactional(readOnly = true)
     EventFullDto getEventById(Long eventId, HttpServletRequest request);
 }
